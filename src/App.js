e31abc5d42ffa4977aae93f7f93a494e8b31cef5
Igor Grinchenko <yurii.kohut@usoftware.co>
2021-12-08 15:00:07 +0200

    Delete all

--- src/App.js
+++ src/App.js
@@ -1,56 +1,7 @@
 import { React } from "react";
-import { connect } from "react-redux";
 
-import { setDecrement, setIncrement, setRandom } from "./action";
-
-const App = ({ setDecrement, setIncrement, setRandom, counter, random }) => {
-  const setDec = () => {
-    setDecrement(counter - 1);
-  };
-
-  const setInc = () => {
-    setIncrement(counter + 1);
-  };
-
-  const setRnd = () => {
-    setRandom(Math.floor(Math.random() * 100));
-  };
-
-  return (
-    <div
-      className="App"
-      style={{
-        textAlign: "center",
-        fontSize: "50px",
-      }}
-    >
-      <h1>Counter: {counter}</h1>
-      <button onClick={setDec} style={{ padding: "20px 40px" }}>
-        DEC
-      </button>
-      <button onClick={setInc} style={{ padding: "20px 40px" }}>
-        INC
-      </button>
-
-      <h2>Random:{random}</h2>
-      <button onClick={setRnd} style={{ padding: "20px 40px" }}>
-        RND
-      </button>
-    </div>
-  );
+const App = () => {
+  return <div className="App"></div>;
 };
 
-const mapStateToProps = (state) => {
-  return {
-    counter: state.reducer.counter,
-    random: state.randomReducer.random,
-  };
-};
-
-const mapDispatchToProps = {
-  setIncrement,
-  setDecrement,
-  setRandom,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
-import reducer from "./reducer";
-import randomReducer from "./randomReducer";
-
-const store = createStore(combineReducers({ reducer, randomReducer }));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
